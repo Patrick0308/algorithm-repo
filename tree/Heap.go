e64ffb1d85bde56adc4ca798cecf61c06c718303
Patrick <dreamlike.sky@foxmail.com>
2019-12-07 20:27:46 +0800

    Heap removeMax

--- tree/Heap.go
+++ tree/Heap.go
@@ -32,6 +32,33 @@ func (h *Heap) Insert(a int) bool {
 	return true
 }
 
+func (h *Heap) RemoveMax() bool {
+	if h.count == 0 {
+		return false
+	}
+	h.data[1] = h.data[h.count]
+	h.count = h.count - 1
+	h.heapify(1, h.count)
+	return true
+}
+
+func (h *Heap) heapify(start int, end int) {
+	for {
+		max := start
+		if start*2 <= end && h.data[start] < h.data[start*2] {
+			max = start * 2
+		}
+		if start*2+1 <= end && h.data[max] < h.data[start*2+1] {
+			max = start*2 + 1
+		}
+		if max == start {
+			break
+		}
+		h.swap(max, start)
+		start = max
+	}
+}
+
 func (h *Heap) swap(a int, b int) {
 	tmp := h.data[a]
 	h.data[a] = h.data[b]
--- tree/Heap_test.go
+++ tree/Heap_test.go
@@ -9,5 +9,15 @@ func TestHeap_Insert(t *testing.T) {
 	h.Insert(8)
 	h.Insert(9)
 	h.Insert(0)
+}
 
+func TestHeap_RemoveMax(t *testing.T) {
+	h := NewHeap(6)
+	h.Insert(5)
+	h.Insert(2)
+	h.Insert(8)
+	h.Insert(9)
+	h.Insert(0)
+	h.RemoveMax()
+	h.RemoveMax()
 }
