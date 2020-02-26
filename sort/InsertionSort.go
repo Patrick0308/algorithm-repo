d5162108bb72f2ee78dfe213fed2bfd994dc2104
Patrick <dreamlike.sky@foxmail.com>
2020-02-26 11:44:21 +0800

    Insertion Sort algorithm

---
+++ sort/InsertionSort.go
@@ -0,0 +1,19 @@
+package sort
+
+func InsertionSort(a []int, n int) {
+	if n <= 1 {
+		return
+	}
+	for i := 1; i < n; i++ {
+		value := a[i]
+		j := i - 1
+		for ; j >= 0; j-- {
+			if a[j] > value {
+				a[j+1] = a[j]
+			} else {
+				break
+			}
+		}
+		a[j+1] = value
+	}
+}
